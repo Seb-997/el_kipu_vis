c7033e9e981e1c2abe2f0b9c0ace13234f47ce6c
SebDataDev <spolis.92@gmail.com>
2019-12-17 19:23:51 +0000

    rutas 2.0

--- peru-rutas.R
+++ peru-rutas.R
@@ -2,3 +2,12 @@ library(dplyr)
 library(tidyr)
 library(httr)
 library(ggplot2)
+
+
+nodes <- httr::GET('https://www.overpass-api.de/api/interpreter?data=[out:json];node(id:497082163);out;', 
+               accept_json())%>%
+               content("parsed")
+
+
+
+
